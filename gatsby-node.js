8b06b2f58768a3092e8102f2bac824d3f8881c91
Jon Sadka <jonsadka@gmail.com>
2020-04-10 22:30:53 -0700

    (chore) Add warnings for missing options

---
+++ gatsby-node.js
@@ -0,0 +1,12 @@
+exports.onPreInit = ({reporter}, options) => {
+  if (!options.publicAPIKey) {
+    reporter.warn(
+      'The Affirm plugin requires a public API key. Did you mean to add it?'
+    );
+  }
+  if (!options.environmentScript) {
+    reporter.warn(
+      'The Affirm plugin requires the path to an Affirm environment script. Did you mean to add it?'
+    );
+  }
+};
